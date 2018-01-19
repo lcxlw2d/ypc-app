d80bf9c2bb696622166f11f697b376c2249ac8a1
lichunxiao <17686547266@163.com>
2018-01-19 13:36:43 +0800

    将main.js中的store提取到redux/store目录下，middleware也进行了提取

--- src/main.js
+++ src/main.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom'
 import App from './containers/App'
 import { AppContainer } from 'react-hot-loader'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-import reducer from './redux/reducers/index'
+// import { createStore, applyMiddleware } from 'redux'
 
-const loggerMiddleware = createLogger()
+// import thunkMiddleware from 'redux-thunk'
+// import { createLogger } from 'redux-logger'
 
-const store = createStore(reducer,applyMiddleware(
-    thunkMiddleware, // 允许我们 dispatch() 函数
-    loggerMiddleware // 一个很便捷的 middleware，用来打印 action 日志
-))
+// import reducer from './redux/reducers/index'
+
+// const loggerMiddleware = createLogger()
+
+// const store = createStore(reducer,applyMiddleware(
+//     thunkMiddleware, // 允许我们 dispatch() 函数
+//     loggerMiddleware // 一个很便捷的 middleware，用来打印 action 日志
+// ))
+
+import store from './redux/store/index'
 
 
 const render = Component => {
--- src/redux/store/middlewares.js
+++ src/redux/store/middlewares.js
@@ -1,5 +1,11 @@
 import { applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+const loggerMiddleware = createLogger()
 
 export default applyMiddleware(
   // you can apply you middleware here
+  thunkMiddleware,
+  loggerMiddleware
 )
